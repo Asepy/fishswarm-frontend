94c0826fe06deabd56c67f6596e891dde31892be
Matt Azucas <matias_azucas@hotmail.com>
2021-03-29 22:21:40 -0400

    Cargado Forms

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,63 +1,61 @@
 import React, {Fragment, useContext, useEffect, useState} from "react";
+import checkMember from "../api/checkMember";
 
-class Search extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        data: []
-      };
-    }
-  
-    componentDidMount() {
-       fetch(" https://cors-anywhere.herokuapp.com/https://pvukdj3jak.execute-api.sa-east-1.amazonaws.com/dev/api/asepy/members/123456",{
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "Authorization": `Bearer: none`,
-          "Content-Type": "application/json",
-        },
-        })
-        .then(res => res.json())
+
+const Search = (props) =>{
+  // Declarar estado
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data , setData ] = useState([]);
+
+
+  // Declrar effects
+  useEffect( () =>  {
+    const memberId = props.cedula;
+    console.log(memberId + 'Llego la cedula')
+       checkMember(memberId)
         .then(
           (result) => {
-            this.setState({
-              isLoaded: true,
-              data: JSON.stringify(result.data)             
-            });
+            // this.setState({
+            //   isLoaded: true,
+            //   data: JSON.stringify(result.data)             
+            // });
+            setIsLoaded(true);
+            setData(result.data );
           },
           // Nota: es importante manejar errores aquí y no en 
           // un bloque catch() para que no interceptemos errores
           // de errores reales en los componentes.
           (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
+            // this.setState({
+            //   isLoaded: true,
+            //   error
+            // });
+            setIsLoaded(true);
+            setError(error);
           }
         )
-    }
-  
-    render() {
-      const { error, isLoaded, data } = this.state;
-      if (error) {
-        return(
-        <div>
-           Error: {error.message}
-          <h2>
-            Data = {data}
-          </h2>
-        </div>
-       );
+  } , [] );
 
-      } else if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-          <h2>{data}</h2>
-        );
-      }
-    }
+  if (error !== null) {
+    return(
+    <div>
+       Error: {error.message}
+      <h2>
+        Data = {data}
+      </h2>
+    </div>
+   );
+
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <h2>{JSON.stringify(data)}</h2>
+      
+    );
   }
+}
+
+
 export default Search;
