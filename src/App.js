c070f3c3cd4b95d109e28580ebec0bbdc4529671
Manuel Chavez <manu36chavez@gmail.com>
2021-03-31 22:59:45 -0400

    Refactorizacion del componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,56 +1,38 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useParams
-} from "react-router-dom";
-import Search from './components/Search';
-import Formulario from './components/Forms'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Formulario from "./components/Formulario";
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/show">Show Members</Link>
-            </li>
-            <li>
-              <Link to="/check">Check Member</Link>
-            </li>
-            <li>
-              <Link to="/register">Register Member</Link>
-            </li>
-          </ul>
-        </nav>
+    <>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/show">Show Members</Link>
+              </li>
+              <li>
+                <Link to="/check">Check Member</Link>
+              </li>
+              <li>
+                <Link to="/register">Register Member</Link>
+              </li>
+            </ul>
+          </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/show" children={<Formulario/>}/>
-          <Route path="/check" children={<Child/>}/>
-          <Route path="/register" children={<Child/>}/>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-
-function Child() {
-  // We can use the `useParams` hook here to access
-  // the dynamic pieces of the URL.
-  let { id } = useParams();
-
-  return (
-    <div>
-      <h2>Prueba</h2>
-    </div>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/show" children={<Formulario />} />
+            <Route path="/check" />
+            <Route path="/register" />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/api/checkMember.js
+++ src/api/checkMember.js
@@ -1,11 +1,11 @@
-export default function checkMember(memberId = "123456") {
-    return fetch(`${process.env.REACT_APP_API_BASE}/members/${memberId}`,{
-          method: "GET",
-          mode: "cors",
-          headers: {
-            "Authorization": `Bearer: none`,
-            "Content-Type": "application/json",
-          },
-          })
-          .then(res => res.json());
-  }
+export default function checkMember(memberId) {
+  console.log("Dentro de checkMember(): " + memberId);
+  return fetch(`${process.env.REACT_APP_API_BASE}/members/${memberId}`, {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      Authorization: `Bearer: none`,
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.json());
+}
