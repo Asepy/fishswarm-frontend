5cc886bf50cb1e681a2fdb8b7707df583f97af63
Matt Azucas <matias_azucas@hotmail.com>
2021-04-11 10:30:48 -0400

    Funcionalidades nuevas

--- src/api/postMembers.js
+++ src/api/postMembers.js
@@ -1,10 +1,10 @@
-export default function postMember(nuevoSocio) {
+export default function postMember(nuevoSocio,birthdate_changed) {
     return fetch(`${process.env.REACT_APP_API_POST}`,{
           method: "POST",
-          mode: "cors",
+          mode: "no-cors",
           body: JSON.stringify({
             name: nuevoSocio.nombre,
-            document: nuevoSocio.document,
+            document: nuevoSocio.documento,
             surname: nuevoSocio.surname,
             birthdate: nuevoSocio.birthdate
             }),
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,8 +2,12 @@ import React, { Fragment } from "react";
 import { Button, TextField, makeStyles } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Cargador from "./Cargador";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 
 // CSS
+
 const useStyles = makeStyles({
   formBox: {
     flexGrow: 1,
@@ -20,6 +24,8 @@ const useStyles = makeStyles({
   },
 });
 
+
+
 const Registrarse = () => {
   const classes = useStyles();
   const { handleSubmit, handleInputChange, data, result_json } = Cargador();
@@ -34,7 +40,7 @@ const Registrarse = () => {
         spacing={2}
       >
         <Grid item xs={10} md={3}>
-          <h1 className={classes.title}> Registrarse</h1>
+          <h1 className={classes.title}> Registro</h1>
           <form className={classes.formBox} onSubmit={handleSubmit}>
             <TextField
               margin="normal"
@@ -59,7 +65,7 @@ const Registrarse = () => {
             <TextField
               margin="normal"
               label="# de C.I."
-              name="document"
+              name="documento"
               placeholder="123456"
               value={data.documento || ""}
               onChange={handleInputChange}
@@ -70,11 +76,166 @@ const Registrarse = () => {
               margin="normal"
               label="Fecha de Nacimiento"
               name="birthdate"
-              placeholder="yyyy-mm-dd"
+              placeholder="mm-dd-yyyy"
               value={data.birthdate || ""}
               onChange={handleInputChange}
               className={classes.textfield}
             />
+           
+            {/* <br></br>
+            <br></br>
+            <InputLabel id="demo-simple-select-label">Sexo</InputLabel>
+              <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={data.sexo}
+
+                      onChange={handleInputChange}
+                    >
+                      <MenuItem value={'Masculino'}>Masculino</MenuItem>
+                      <MenuItem value={'Femenino'}>Femenino</MenuItem>
+                </Select>
+            <br></br>
+            <br></br>
+            <InputLabel id="demo-simple-select-label">Departamento</InputLabel>
+              <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={data.departamento}
+
+                      onChange={handleInputChange}
+                    >
+                      <MenuItem value={'Capital'}>Capital</MenuItem>
+                      <MenuItem value={'Concepción'}>Concepción</MenuItem>
+                      <MenuItem value={'San Pedro'}>Capital</MenuItem>
+                      <MenuItem value={'Coordillera'}>Coordillera</MenuItem>
+                      <MenuItem value={'Guairá'}>Guairá</MenuItem>
+                      <MenuItem value={'Caaguazú'}>Caaguazú</MenuItem>
+                      <MenuItem value={'Caazapá'}>Caazapá</MenuItem>
+                      <MenuItem value={'Itapúa'}>Itapúa</MenuItem>
+                      <MenuItem value={'Misiones'}>Misiones</MenuItem>
+                      <MenuItem value={'Paraguarí'}>Paraguarí</MenuItem>
+                      <MenuItem value={'Alto Paraná'}>Alto Paraná</MenuItem>
+                      <MenuItem value={'Central'}>Central</MenuItem>
+                      <MenuItem value={'Ñeembucú'}>Ñeembucú</MenuItem>
+                      <MenuItem value={'Amambay'}>Amambay</MenuItem>
+                      <MenuItem value={'Canindeyú'}>Canindeyú</MenuItem>
+                      <MenuItem value={'Pdte. Hayes'}>Pdte. Hayes</MenuItem>
+                      <MenuItem value={'Alto Paraguay'}>Alto Paraguay</MenuItem>
+              </Select>
+            <br></br>
+            <TextField
+              margin="normal"
+              label="Ciudad"
+              name="city"
+              placeholder="Ciudad"
+              value={data.city || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <br></br>
+            <TextField
+              margin="normal"
+              label="Email"
+              name="mail"
+              placeholder="correo@gmail.com"
+              value={data.mail || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <br></br>
+            <TextField
+              margin="normal"
+              label="Celular"
+              name="celular"
+              placeholder="0981000000"
+              value={data.celular || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <InputLabel id="demo-simple-select-label">Registro del Emprendimiento</InputLabel>
+            <br></br>
+            <TextField
+              margin="normal"
+              label="Razon social"
+              name="razonsocial"
+              placeholder="Razón social"
+              value={data.razonsocial || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <br></br>
+            <TextField
+              margin="normal"
+              label="Nombre de Fantasía"
+              name="nfantasia"
+              placeholder="Nombre de fantasia"
+              value={data.nfantasia || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <br></br>
+            <TextField
+              margin="normal"
+              label="R.U.C"
+              name="ruc"
+              placeholder="7777777-3"
+              value={data.ruc || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <InputLabel id="demo-simple-select-label"> * El único requisito para asociarte es contar con un RUC activo</InputLabel>
+            <br></br>
+            <TextField
+              margin="normal"
+              label="Especifique el rubro"
+              name="rubro"
+              placeholder="Rubro"
+              value={data.rubro || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <br></br>
+            <TextField
+              margin="normal"
+              label=" Cantidad de empleados (si no aplica: 0)"
+              name="empleados"
+              placeholder="7"
+              value={data.empleados || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <br></br>
+            <br></br>
+            <InputLabel id="demo-simple-select-label">Facturación del 2019</InputLabel>
+              <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={data.faturación}
+
+                      onChange={handleInputChange}
+                    >
+                      <MenuItem value={'Menor o igual a 650 millones Gs.'}>Menor o igual a 650 millones Gs.</MenuItem>
+                      <MenuItem value={'Entre 650 millones y 3.250 millones de Gs.'}>Entre 650 millones y 3.250 millones de Gs.</MenuItem>
+                      <MenuItem value={'Entre 3.250 millones y 7.700 millones de Gs.'}>Entre 3.250 millones y 7.700 millones de Gs.</MenuItem>
+                      <MenuItem value={'Mayor a 7.700 millones Gs.'}>Mayor a 7.700 millones Gs.</MenuItem>
+                </Select>
+            <br></br>
+            <TextField
+              margin="normal"
+              label=" Sitio web o redes sociales"
+              name="sitioweb"
+              placeholder="www.asepy.com"
+              value={data.sitioweb || ""}
+              onChange={handleInputChange}
+              className={classes.textfield}
+            />
+            <br></br>
+            <br></br> */}
             <Button
               className={classes.button}
               type="submit"
