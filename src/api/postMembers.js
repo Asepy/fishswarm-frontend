08cdb883eac422097b5519d1ee7fa618a626b4f2
Matt Azucas <matias_azucas@hotmail.com>
2021-04-11 10:39:52 -0400

    Post member actualizado

--- src/api/postMembers.js
+++ src/api/postMembers.js
@@ -1,7 +1,7 @@
 export default function postMember(nuevoSocio,birthdate_changed) {
     return fetch(`${process.env.REACT_APP_API_POST}`,{
           method: "POST",
-          mode: "no-cors",
+          mode: "cors",
           body: JSON.stringify({
             name: nuevoSocio.nombre,
             document: nuevoSocio.documento,
