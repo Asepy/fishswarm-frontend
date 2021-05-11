19a171c898c403ad7b3c7a0caf1e68787ea6e842
Rodrigo Villalba <rodris.jvo@gmail.com>
2021-05-11 11:02:07 -0400

    Agrega cloudfront id único para deploy

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports = {
   env: {
     NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE,
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    CF_DISTRIBUTION_ID: process.env.CF_DISTRIBUTION_ID,
   },
 };
