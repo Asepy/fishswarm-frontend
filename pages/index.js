3bfc550613ab28062c10b0c785a86bb5b856eeb7
Rodrigo Villalba <rodris.jvo@gmail.com>
2021-05-14 11:48:43 -0400

    Hace responsive inputs de form registro miembros

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, GridItem } from "@chakra-ui/layout";
 import Container from "components/ui/Container";
 import RegisterForm from "components/members/RegisterForm";
 import PublicLayout from "components/ui/layout/PublicLayout";
@@ -7,7 +8,20 @@ export default function index() {
   return (
     <PublicLayout>
       <Container>
-        <RegisterForm />
+        <Grid
+          templateColumns={{
+            base: "repeat(1, minmax(auto, 1fr))",
+            md: "repeat(1, minmax(auto, 1fr))",
+            xl: "repeat(5, minmax(auto, 1fr))",
+          }}
+          gap={4}
+          pb="16"
+        >
+          <GridItem colSpan={{ xl: 3 }}>
+            <RegisterForm />
+          </GridItem>
+          <GridItem />
+        </Grid>
       </Container>
     </PublicLayout>
   );
