f72a49d5db9574c07dfe866364d956277732801c
Rodrigo Villalba <rodris.jvo@gmail.com>
2021-05-23 20:52:47 -0400

    Limpieza de código en useFilterMember

--- utils/useFilterMember.js
+++ utils/useFilterMember.js
@@ -1,14 +1,17 @@
-import React from "react";
-import { useQuery, useQueryClient } from "react-query";
+import { useState, useMemo } from "react";
+import { useQuery } from "react-query";
 import serialize from "./serialize";
+
+export const FILTER_MEMBER_QUERY_ID = "query:filter-members";
+export const FILTER_MEMBER_PAGED_QUERY_ID = "query:filter-members-paginated";
 
 async function filterMember({ page, name, document }) {
   const queryParams = serialize({ page, name, document });
   const response = await fetch(`/api/members/filter?${queryParams}`, {
     method: "GET",
     headers: {
-      "Content-Type": "application/json",
-    },
+      "Content-Type": "application/json"
+    }
   });
   if (!response.ok) {
     const errorJson = await response.json();
@@ -17,15 +20,13 @@ async function filterMember({ page, name, document }) {
   return response.json();
 }
 
-export const FILTER_MEMBER_QUERY_ID = "query:filter-members";
-export const FILTER_MEMBER_PAGED_QUERY_ID = "query:filter-members-paginated";
-
-export default function useFilterMember({ page }, options = {}) {
+export default function useFilterMember({ page }) {
   return useQuery([FILTER_MEMBER_QUERY_ID, page], () => filterMember({ page }));
 }
 
-export function useFilterMemberPaginated(queryParams, options = {}) {
-  const [page, setPage] = React.useState(1);
+export function useFilterMemberPaginated(queryParams) {
+  const [page, setPage] = useState(1);
+
   const { name, document } = queryParams;
   const { data, ...restQuery } = useQuery(
     [FILTER_MEMBER_PAGED_QUERY_ID, page, name, document],
@@ -33,14 +34,15 @@ export function useFilterMemberPaginated(queryParams, options = {}) {
     { keepPreviousData: true }
   );
 
-  const hasMore = React.useMemo(
+  const hasMore = useMemo(
     () => page < data?.pageTotal && data?.pageTotal < data?.total,
     [data]
   );
+
   const previousPage = () => setPage((old) => Math.max(old - 1, 0));
+
   const nextPage = () => setPage((old) => (hasMore ? old + 1 : old));
 
-  console.log({ data });
   return {
     hasMore,
     nextPage,
@@ -48,6 +50,6 @@ export function useFilterMemberPaginated(queryParams, options = {}) {
     setPage,
     previousPage,
     data,
-    ...restQuery,
+    ...restQuery
   };
 }
