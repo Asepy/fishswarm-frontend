3f0895887f50362f6eb35bcb03ec7873f703e13b
Rodrigo Villalba <rodris.jvo@gmail.com>
2021-06-05 12:19:51 -0400

    Modifica color de estado pendiente

--- utils/getUIMemberStatus.js
+++ utils/getUIMemberStatus.js
@@ -1,8 +1,8 @@
 const statusMap = {
   ACTIVE: { label: "Activo", color: "green" },
   INACTIVE: { label: "Inactivo", color: "gray" },
-  PENDING: { label: "Pendiente", color: "red" },
-  CONDITIONAL: { label: "Condicional", color: "orange" },
+  PENDING: { label: "Pendiente", color: "cyan" },
+  CONDITIONAL: { label: "Condicional", color: "orange" }
 };
 
 export default function getUIMemberStatus(status) {
