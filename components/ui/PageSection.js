1ea395c693f01d99ed90083bd94ae7cd504ac217
Rodrigo Villalba <rodris.jvo@gmail.com>
2021-07-14 15:08:23 -0400

    Extrae componente PageSection para secciones

---
+++ components/ui/PageSection.js
@@ -0,0 +1,6 @@
+import { Stack } from "@chakra-ui/react";
+import React from "react";
+
+export default function PageSection(props) {
+  return <Stack bg="white" boxShadow="md" borderRadius="lg" {...props} />;
+}
