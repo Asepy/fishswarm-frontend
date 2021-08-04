1a2c6218ab86d5643f4437a582f3700c0c45309f
Rodrigo Villalba <rodris.jvo@gmail.com>
2021-08-04 14:14:35 -0400

    Agrega config de trailing spaces

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   target: "serverless",
+  trailingSlash: true,
   env: {
     NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE,
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
-    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
-  },
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL
+  }
 };
