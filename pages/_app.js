b5634e18c3c9ebb469a82f90a847e43d8e59db52
Manuel Chavez <manu36chavez@gmail.com>
2022-01-20 11:20:58 -0300

    reemplazar gtm tags en duro por lib: react-gtm-module

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import React from "react";
+import { React, useEffect } from "react";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import customTheme from "styles/theme";
+import TagManager from "react-gtm-module";
 import "../polyfills";
 import "react-datepicker/dist/react-datepicker.css";
 import "styles/react-datepicker.css";
@@ -11,6 +12,12 @@ import "styles/react-datepicker.css";
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_KEY
+    });
+  });
+
   return (
     <>
       <ChakraProvider theme={customTheme}>
